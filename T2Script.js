3fec0e3e7d1a95dcebc054359ad67ee45b447cfa
Anshuman271 <145823800+Anshuman271@users.noreply.github.com>
2023-11-09 19:43:36 +0530

    Create T2Script.js

---
+++ T2Script.js
@@ -0,0 +1,4 @@
+// script.js - You can add JavaScript functionality here if needed
+document.addEventListener("DOMContentLoaded", function() {
+  // Add JavaScript code here if you have any interactivity or dynamic content.
+});
